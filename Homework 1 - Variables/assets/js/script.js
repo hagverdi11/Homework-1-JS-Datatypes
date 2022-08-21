5f8111721c8aa3f0f8a704f7903e042b2d42cc94
hagverdi11 <hagverdiam@code.edu.az>
2022-08-21 19:10:24 +0400

    Added homework

---
+++ Homework 1 - Variables/assets/js/script.js
@@ -0,0 +1,71 @@
+// alert("Hello")
+
+let name = "Filankes";
+let age = 55;
+
+// console.log(`${name} ${age}`);
+
+// let person = {
+//     name: "Orxan",
+//     surname: "Ekberov",
+//     age: 26,
+//     numbers: [545445,454545]
+// }
+
+// let person1 = {
+//     name: "Eli",
+//     surname: "Veli",
+//     age: 85,
+//     numbers: [89281881,828284466]
+// }
+
+// console.log(person.name + " " + person.surname + " " + person.age);
+
+// let students = ["Orxan", "Huseyn", 55];
+// for (let i = 0; i < students.length; i++); 
+//  console.log(students[i]); 
+
+//  let sagirds = ["ali", "vali", 45];
+//  for (let i = 0; i < sagirds.length; i++) {
+    
+//     console.log(sagirds[i]);
+
+    
+//  }
+
+// let students = [person, person1];
+// for (let i = 0; i < person.numbers.length; i++)
+
+// console.log(person.numbers[i]); 
+
+// let numbers = [5,10,57,89,58]
+// numbers.forEach(element => {
+//     console.log(element)
+// });
+
+
+// for (let i = 0; i <= 10; i++) {
+//     if(i%2==1){
+//         console.log("tek" + " " + i)
+//     }
+
+//     else{
+//         console.log("cut" + " " + i)
+//     }
+    
+// }
+
+// let isActive = true;
+
+// if(isActive){
+//     console.log("Aktiv deyil");
+// }
+// else{
+//     console.log("Aktivdir.");
+// }
+
+let studentSurname = null;
+
+let result = studentSurname ?? "nulldur"
+
+console.log(typeof result);
